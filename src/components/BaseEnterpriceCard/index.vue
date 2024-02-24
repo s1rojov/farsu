de1bed1264e683e46d296296a1324dd8a5b28222
unknown <you@example.comhusniddinxomidov2002@gmail.com>
2024-02-24 23:27:09 +0500

    added companycard and productcard

---
+++ src/components/BaseEnterpriceCard/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import BaseIcon from 'src/components/BaseIcon/index.vue'
+import BaseButton from 'src/components/BaseButton/index.vue'
+const props = defineProps({
+  cardData: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+<template>
+  <div class="rounded-[20px] bg-white p-5">
+    <img :src="props.cardData.img" alt="" class="w-full rounded-2xl overflow-hidden object-cover h-1/2">
+    <div class="flex items-center gap-[15px]">
+      <img :src="props.cardData.logo" alt="" class="w-12 h-12 rounded-full object-cover overflow-hidden">
+      <div class="pt-[15px] pb-7">
+        <p class="text-zinc-900 text-lg font-extrabold leading-tight">{{ props.cardData.name }}</p>
+        <p class="text-zinc-900 text-xs font-normal leading-[15px] flex gap-1">{{ props.cardData.subtitle }}
+          <BaseIcon name="star" class="w-[14px] h-[14px] object-cover text-[#E9D410]" /><span class="font-semibold">{{ props.cardData.rate }}</span>/5
+        </p>
+      </div>
+      <BaseButton color="green" label="Batafsil" class="py-[10px] w-full" />
+    </div>
+  </div>
+</template>
+<style scoped></style>
---
+++ src/components/BaseProductCard/index.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import BaseIcon from 'src/components/BaseIcon/index.vue'
+import BaseButton from 'src/components/BaseButton/index.vue'
+const props = defineProps({
+  cardData: {
+    type: Object, 
+    required: true
+  }
+})
+</script>
+<template>
+  <div class="px-[10px] py-[18px] rounded-[20px] bg-white h-auto">
+    <div class="w-full h-[211px] object-cover">
+      <img :src="props.cardData.img" alt="" class="h-full w-full  object-cover overflow-hidden rounded-2xl">
+    </div>
+    <div>
+      <p class="text-gray-800 text-base font-bold leading-relaxed pt-[18px] pb-[6px]"><span>{{ props.cardData.price }}</span> so'm</p>
+      <p class="text-zinc-500 text-sm leading-tight font-normal">{{ props.cardData.title }}</p>
+      <p class="text-emerald-600 text-opacity-95 text-xs font-medium leading-4 pt-[13px] pb-[18px]">{{ props.cardData.company }}
+      </p>
+      <div class="flex items-end gap-[2px]">
+        <BaseIcon name="star" class="text-orange-400 w-4 h-4 object-cover" v-for="index in 5" :key="index" />
+        <p class="text-zinc-500 text-xs font-medium leading-[14px]"><span>{{ props.cardData.star }}</span></p>
+      </div>
+      <div class="pt-[19px] flex justify-between w-full">
+        <BaseButton color="green" label="So'rov yuborish"
+          class="py-2 rounded-[9px] w-[83%] flex items-center justify-center" />
+        <BaseIcon name="like" class=" bg-emerald-600 w-[15%] h-auto rounded-[9px] p-2 cursor-pointer" />
+      </div>
+    </div>
+  </div>
+</template>

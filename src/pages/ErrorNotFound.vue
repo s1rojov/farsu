aa63a80c5c04538520ac61451cb2a0af6a9ee5a3
Jahongirf04 <jahongirf04@gmail.com>
2024-03-01 19:40:12 +0500

    not found page

--- src/pages/ErrorNotFound.vue
+++ src/pages/ErrorNotFound.vue
@@ -1,27 +1,31 @@
 <template>
-  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
-    <div>
-      <div style="font-size: 30vh">
-        404
-      </div>
+  <img class="absolute top-[150px] left-[80px]" src="/images/some-stat1.png" alt="pic">
+  <img class="absolute bottom-[150.66px] left-[180.22px]" src="/images/some-stat2.png" alt="pic">
+  <img class="absolute top-[150.66px] right-[110.22px]" src="/images/some-stat3.png" alt="pic">
+  <img class="absolute bottom-[150.66px] right-[80.22px]" src="/images/some-stat4.png" alt="pic">
 
-      <div class="text-h2" style="opacity:.4">
-        Oops. Nothing here...
-      </div>
+  <div class="fullscreen bg-nero text-white text-center q-pa-md flex flex-center flex-col font-medium">
+    <img :src="greenLogo" alt="">
 
-      <q-btn
-        class="q-mt-xl"
-        color="white"
-        text-color="blue"
-        unelevated
-        to="/"
-        label="Go Home"
-        no-caps
-      />
-    </div>
+
+      <p class="text-[#1D3448] text-[30px] mt-[40px]">Siz izlayotgan sahifa topilmadi(</p>
+      <p class="text-[#78818A] text-[15px]">Siz izlayotgan sahifa topilmadi(</p>
+
+    <img :src="error404" alt="">
+    
+    <q-btn
+    class="q-mt-xl bg-[#1B9468] w-[230px] h-[63px] rounded-[43px]"
+    text-color="white"
+    unelevated
+    to="/"
+    label="Bosh sahifaga qaytish"
+    no-caps
+    />
   </div>
 </template>
 
 <script setup lang="ts">
+import greenLogo from '/svgs/green-logo.svg'
+import error404 from '/svgs/404-error.svg'
 
 </script>
